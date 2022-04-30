e00a291c89a5adc04d2dec60945979c5e5fa12b1
hyeonsik <dgt4862@gmail.com>
2022-04-30 22:41:18 +0900

    date_picker 를 활용한 날짜 선택 및 타이머

--- DatePicker_Mission/ViewController.swift
+++ DatePicker_Mission/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // 타이머가 구동되면 실행할 함수를 지정
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
 
+    
+    @IBOutlet var nowTimeLabel: UILabel!
+    
+    @IBOutlet var selectTimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        selectTimeLabel.text = "선택된 시간: " + formatter.string(from: datePickerView.date)
+        
+    }
+    
+    @objc func updateTime() {
+        nowTimeLabel.text = String(count)
+        count = count + 1
+    }
+    
 }
 
