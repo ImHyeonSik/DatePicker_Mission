150e1692117ea877858a154b8385e657e93b4c2b
hyeonsik <dgt4862@gmail.com>
2022-04-30 23:37:31 +0900

    선택한 시간과 현재시간이 같아지면 배경색이 변하고 특정 시간이 지난 후 다시 원색으로 돌아옴

--- DatePicker_Mission/ViewController.swift
+++ DatePicker_Mission/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 class ViewController: UIViewController {
     // 타이머가 구동되면 실행할 함수를 지정
     let timeSelector: Selector = #selector(ViewController.updateTime)
+    let colorSelector: Selector = #selector(ViewController.updateColor)
     let interval = 1.0
+    let oneMin = 10.0
     var count = 0
+    var alarmTime : String?
 
     
     @IBOutlet var nowTimeLabel: UILabel!
@@ -32,6 +35,9 @@ class ViewController: UIViewController {
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
         selectTimeLabel.text = "선택된 시간: " + formatter.string(from: datePickerView.date)
         
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+        
     }
     
     @objc func updateTime() {
@@ -43,6 +49,18 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         nowTimeLabel.text = "현재시간 : " + formatter.string(from: date as Date)
+    
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        if (currentTime == alarmTime) {
+            view.backgroundColor = UIColor.red
+            Timer.scheduledTimer(timeInterval: oneMin, target: self, selector: colorSelector, userInfo: nil, repeats: false)
+        }
+    }
+    
+    @objc func updateColor() {
+        
+        view.backgroundColor = UIColor.white
     }
     
 }
